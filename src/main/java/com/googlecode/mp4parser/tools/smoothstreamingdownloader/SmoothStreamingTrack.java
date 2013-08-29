889f7b8843417b5d599b801f75d5a24f758afb2f
sebastian.annies@gmail.com <sebastian.annies@gmail.com@7decde4b-c250-0410-a0da-51896bc88be6>
2013-08-29 12:10:10 +0000

    Anton's patch dealing with high mem consumption removes the ByteBuffer as sample representation and introduces a Sample class as adapter between ByteBuffer and external components
    git-svn-id: https://mp4parser.googlecode.com/svn/trunk/tools/smooth-streaming-downloader@935 7decde4b-c250-0410-a0da-51896bc88be6

--- src/main/java/com/googlecode/mp4parser/tools/smoothstreamingdownloader/SmoothStreamingTrack.java
+++ src/main/java/com/googlecode/mp4parser/tools/smoothstreamingdownloader/SmoothStreamingTrack.java
@@ -13,6 +13,7 @@ import com.coremedia.iso.boxes.VideoMediaHeaderBox;
 import com.coremedia.iso.boxes.fragment.TrackRunBox;
 import com.coremedia.iso.boxes.h264.AvcConfigurationBox;
 import com.coremedia.iso.boxes.sampleentry.VisualSampleEntry;
+import com.googlecode.mp4parser.authoring.Sample;
 import com.googlecode.mp4parser.authoring.Track;
 import com.googlecode.mp4parser.authoring.TrackMetaData;
 import com.googlecode.mp4parser.util.Path;
@@ -202,7 +203,7 @@ public class SmoothStreamingTrack implements Track {
     }
 
     @Override
-    public List<ByteBuffer> getSamples() {
+    public List<Sample> getSamples() {
 
         // new FragmentedMp4SampleList()
         return null;
